ffa0f0ae5dee77c1fce58673f8411e754151251d
Praneeth <johndoeknown@protonmail.com>
2020-07-22 14:22:09 +0530

    added ToolTipButton

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
     import SlowFloatCard from "./components/SlowFloatCard.svelte";
+    import ToolTipButton from "./components/ToolTipButton.svelte";
 </script>
 
 <SlowFloatCard horizontal="true">
@@ -9,3 +10,7 @@
 <SlowFloatCard>
     <h1>Hello World</h1>
 </SlowFloatCard>
+
+<ToolTipButton title="Show ToolTip" on:click={() => window.alert("Hello World")} duration={1500}>
+    This is a ToolTip!
+</ToolTipButton>
---
+++ src/components/ToolTipButton.svelte
@@ -0,0 +1,39 @@
+<script>
+    import { fade } from "svelte/transition";
+
+    let show = false;
+    export let title = "Show Tooltip"
+    export let duration = 1000;
+
+    const toggleShow = () => {
+        show = true;
+        setTimeout(() => show = false, duration);
+    }
+</script>
+
+<button on:mouseover={toggleShow} on:mouseleave={() => show = false} on:click>{title}</button>
+{#if show}
+    <p transition:fade><slot/></p>
+{/if}
+
+<style>
+    button {
+        margin: 0;
+        padding: 15px;
+        font-size: 20px;
+        cursor: pointer;
+        background-color: white;
+        border: none;
+        box-shadow: 0 10px 10px lightgray;
+    }
+
+    p {
+        margin: 10px;
+        padding: 10px;
+        background-color: black;
+        color: white;
+        width: 10%;
+        font-size: 15px;
+        font-family: Consolas, monospace;
+    }
+</style>
